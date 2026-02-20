a5ad6335fd5a40a62ac56bb5b66e8d07041d710f
RajarshiKarmakar1604 <rajarshikarmakar1604@gmail.com>
2026-02-20 12:13:04 +0530

    changes to main.py

--- Backend/App/main.py
+++ Backend/App/main.py
@@ -14,11 +14,11 @@ from spotify import SpotifyClient
 
 app = FastAPI(title="Spotify Comparability API")
 
-FRONTEND_URL = "https://vibe-sync-navy.vercel.app/"
+FRONTEND_URL = "https://vibe-sync-navy.vercel.app"
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL, "http://localhost:3000"],
+    allow_origins=[FRONTEND_URL, "https://vibe-sync-navy.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
